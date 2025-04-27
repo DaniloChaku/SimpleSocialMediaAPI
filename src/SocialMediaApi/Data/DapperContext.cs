4a8a3ed0efec477f717c6d21cc8449a14f3db8fd
danylo <danilochaku@gmail.com>
2025-04-27 14:21:32 +0300

    Add DapperContext for database interactions

---
+++ src/SocialMediaApi/Data/DapperContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+using Npgsql;
+
+namespace SocialMediaApi.Data;
+
+public class DapperContext
+{
+    private readonly string _connectionString;
+
+    public DapperContext(IConfiguration config)
+    {
+        _connectionString = config.GetConnectionString("DefaultConnection")
+            ?? throw new InvalidOperationException("Connection string not found");
+    }
+
+    public IDbConnection CreateConnection() => new NpgsqlConnection(_connectionString);
+}
--- src/SocialMediaApi/Program.cs
+++ src/SocialMediaApi/Program.cs
@@ -1,4 +1,8 @@
+using SocialMediaApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<DapperContext>();
 
 var app = builder.Build();
 
