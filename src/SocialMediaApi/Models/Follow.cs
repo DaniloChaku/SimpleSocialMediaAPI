a9a9bd17fa6c4ebcb16c19b0e7e600e146eaef9c
danylo <danilochaku@gmail.com>
2025-04-27 11:31:35 +0300

    Add model classes

---
+++ src/SocialMediaApi/Models/Follow.cs
@@ -0,0 +1,7 @@
+﻿namespace SocialMediaApi.Models;
+
+public class Follow
+{
+    public int FollowerId { get; set; }
+    public int FollowingId { get; set; }
+}
---
+++ src/SocialMediaApi/Models/Like.cs
@@ -0,0 +1,7 @@
+﻿namespace SocialMediaApi.Models;
+
+public class Like
+{
+    public int UserId { get; set; }
+    public int PostId { get; set; }
+}
